3690b1164b0feae46d92235d38ac730b21a36a1a
ArthurAllioux <arthur.allioux@epfedu.fr>
2021-05-19 17:44:30 +0200

    google analitycs added

--- app.py
+++ app.py
@@ -1,2 +1,22 @@
+import os
+import re
+
+code = """<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-G8LLP1NRE9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-G8LLP1NRE9');
+</script>"""
+
+a=os.path.dirname(st.__file__) + '/static/index.html'
+with open(a, 'r') as f:
+    data=f.read()
+    if len(re.findall('UA-', data)) == 0:
+        with open(a, 'w') as f:
+            newdata=re.sub('<head>','<head>' + code, data)
+            f.write(newdata)
+
 import streamlit as st
 st.title("Hello worl   d !")

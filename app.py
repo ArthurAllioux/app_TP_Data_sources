c2a0cc224359aa73fc2b994538cefef1ce3ad290
ArthurAllioux <arthur.allioux@epfedu.fr>
2021-05-26 16:52:49 +0200

    manipulate cookies

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import logging
+import requests
 
 import os
 import re
@@ -25,3 +26,12 @@ st.title("Hello worl   d ! It's Arthur ALLIOUX")
 input = st.text_input('Variable', 'exemple')
 logging.warning('La variable est ' + input)
 st.write('The value is', input)
+if st.button('Make a Google Analytics request'):
+  req = requests.get("https://www.google.com/")
+  req2 = requests.get('https://analytics.google.com/analytics/web/#/p273040931/reports/defaulthome?params=_u..nav%3Ddefault')
+  st.text(req2.status_code)
+  st.markdown(req2.text)
+  st.write('\n Request Done  ' + str(req))
+  st.markdown(req.cookies._cookies)
+else:
+  st.write('Request not Done')
